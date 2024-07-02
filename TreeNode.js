0a6e3d75961dfefdd2b47d723d642eaccf69ec1e
Mudikonda Rohan Kumar <94850476+R0hankumar@users.noreply.github.com>
2024-07-02 08:19:58 +0530

    Create TreeNode.js

---
+++ TreeNode.js
@@ -0,0 +1,12 @@
+// components/TreeNode.js
+import React from 'react';
+
+const TreeNode = ({ node }) => {
+  return (
+    <div>
+      Node Value: {node.value}
+    </div>
+  );
+};
+
+export default TreeNode;
